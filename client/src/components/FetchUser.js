994772b823ccf2107d53ded9762b47552b3a4674
MichaelJDaniel <soldbymichaeld@gmail.com>
2019-01-02 13:46:46 -0700

    add component FetchUser

--- client/src/components/FetchUser.js
+++ client/src/components/FetchUser.js
@@ -5,6 +5,41 @@ import {AuthConsumer} from "../providers/AuthProvider"
 class FetchUser extends React.Component {
   state = { loaded: false, }
 
+  componentDidMount() {
+    const { auth: { authenticated, handleLogin, }, } = this.props;
+    if (authenticated) {
+      this.loaded();
+    } else {
+      if (this.checkLocalToken()) {
+        axios.get("/api/auth/validate_token")
+        .then( res => {
+          handleLogin(res.data.data);
+          this.loaded();
+        })
+        .catch( res => {
+          console.log(res);
+          this.loaded();
+        })
+      } else {
+        this.loaded();
+      }
+    }
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if (!this.state.loaded) this.loaded();
+  }
+
+  loaded = () => this.setState({ loaded: true, })
+    
+  
+  checkLocalToken = () => {
+      const token = localStorage.getItem("access-token");
+      return token;
+  }
+
+
+
   render() {
     return this.state.loaded ? this.props.children : null;
 
@@ -14,6 +49,9 @@ class FetchUser extends React.Component {
 const ConnectedFetchUser = (props) => (
   <AuthConsumer>
     { auth => 
+      <FetchUser {...props} auth={auth} />
     }
   </AuthConsumer>
 )
+
+export default ConnectedFetchUser;
